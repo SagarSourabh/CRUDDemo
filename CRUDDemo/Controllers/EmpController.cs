134e715c31fc90f885fbad0532b9eb47f624aec7
Sagar <sagarsamal92@gmail.com>
2020-02-16 00:41:51 +0530

    Project Upload To GitHub

---
+++ CRUDDemo/Controllers/EmpController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CRUDDemo.Models;
+
+namespace CRUDDemo.Controllers
+{
+    public class EmpController : Controller
+    {
+        private readonly ApplicationDBContext _DbContext;
+        public EmpController(ApplicationDBContext DB)
+        {
+            _DbContext = DB;
+        }
+        public IActionResult Index()
+        {
+            var employeelist = _DbContext.TblEmployee.ToList();
+            return View(employeelist);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _DbContext.Add(model);
+                _DbContext.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int empID)
+        {
+            EmployeeModel model = new EmployeeModel();
+
+            var employee = _DbContext.TblEmployee.Where(i => i.EmployeeID == empID).FirstOrDefault();
+            if (employee != null)
+            {
+                model.EmployeeID = empID;
+                model.EmployeeName = employee.EmployeeName;
+                model.EmployeeEmail = employee.EmployeeEmail;
+                model.EmployeeAge = employee.EmployeeAge;
+                model.EmployeeSalary = employee.EmployeeSalary;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EmployeeModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var employee = _DbContext.TblEmployee.Where(i => i.EmployeeID == model.EmployeeID).FirstOrDefault();
+
+                employee.EmployeeName = model.EmployeeName;
+                employee.EmployeeEmail = model.EmployeeEmail;
+                employee.EmployeeAge = model.EmployeeAge;
+                employee.EmployeeSalary = model.EmployeeSalary;
+                _DbContext.Update(employee);
+                await _DbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+  
+        }
+    }
+}
---
+++ CRUDDemo/Models/EmployeeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDDemo.Models
+{
+    public class EmployeeModel: IEntity
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage ="Employee name is required")]
+        public string EmployeeName { get; set; }
+
+        [Display(Name ="Email")]
+        [Required(ErrorMessage = "Employee email is required")]
+        public string EmployeeEmail { get; set; }
+
+        [Display(Name = "Age")]
+        [Required(ErrorMessage = "Employee age is required")]
+        [Range(22,58)]
+        public int EmployeeAge { get; set; }
+
+        [Display(Name = "Salary")]
+        [Required(ErrorMessage = "Employee salary is required")]
+        public decimal? EmployeeSalary { get; set; }
+    }
+}
